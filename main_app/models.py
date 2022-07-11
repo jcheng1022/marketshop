e94d49048f596e13c86b803f2a7ff2a1df281cd2
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-07-11 12:06:43 -0400

    adds login/logout functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+CATEGORIES = (
+    ('E','Electronics'),
+    ('T','Toys'),
+    ('H', 'Home Decor'),
+    ('B', 'Beauty'),
+    ('S', 'Sport Items'),
+    ('O', 'Office Items'),
+    ('X', 'Other')
+)
+
+class Item(models.Model):
+    name = models.CharField(max_length = 100)
+    category = models.CharField(
+        max_length = 1,
+        default = CATEGORIES[6][0]
+    )
+    price = models.IntegerField()
+    description = models.TextField(max_length = 350)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 
 def home (request):
     return render(request, 'home.html')
+
+def item_index(request):
+    items = Item.objects.all()
+    return render(request, 'items/index.html', {'items':items})
