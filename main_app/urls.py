1a905bcd1ce3192004c5b7b8ce6765be47a7165d
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-07-11 12:51:22 -0400

    adds signup functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('items/browse/', views.item_index, name='index'),
+    path('items/<int:item_id>', views.items_detail, name ="item_detail"),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -10,3 +12,22 @@ def home (request):
 def item_index(request):
     items = Item.objects.all()
     return render(request, 'items/index.html', {'items':items})
+
+def items_detail(request, item_id):
+    item = Item.object.get(id=item_id)
+    return render(request, 'items/detail.html', {
+        'item':item})
+
+def signup(request):
+    error_message = ''
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid Sign Up, please try again'
+    form = UserCreationForm()
+    context = {'form':form, 'error_message':error_message}
+    return render(request, 'registration/signup.html', context)
