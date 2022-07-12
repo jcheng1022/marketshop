4d0152524d18f945db19a9294fb3506f15acc38a
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-07-12 15:48:28 -0400

    updating code

--- marketshop/settings.py
+++ marketshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^k_a$$4)yas0pgz2z4t-a+k34@fn3xol!$sl*t9@$b!-+_b-(8'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
